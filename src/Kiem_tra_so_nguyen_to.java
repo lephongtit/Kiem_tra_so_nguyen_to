933a5d36b851e44764329a01695ee45ea8acfd91
Le Hoang Thien Phong <thienphong251199@gmail.com>
2019-07-24 08:48:49 +0700

    'oke'

---
+++ src/Kiem_tra_so_nguyen_to.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Kiem_tra_so_nguyen_to {
+    public static void main(String[] args) {
+        Scanner sr=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int number = sr.nextInt();
+        if (number < 2)
+            System.out.println(number + " is not a prime");
+        else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " is a prime");
+            else
+                System.out.println(number + " is not a prime");
+        }
+    }
+}
